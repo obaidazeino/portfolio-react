5a67aebe48e3253ad54687c016f4c1a337bc2801
Obaida Zeino <84151630+obaidazeino@users.noreply.github.com>
2021-12-11 09:05:18 -0500

    intitla commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react"
 import './App.css';
 import Banner from "./components/Banner"
 import ContactForm from "./components/ContactForm"
-import Countries from "./components/Countries"
 import Footer from "./components/Footer"
 import Map from "./components/Map"
 import Nav from "./components/Nav"
 import Quote from "./components/Quote"
 import Section from "./components/Section"
+import writingImg from "./components/images/writing.svg"
+import graphicImg from "./components/images/trees.svg"
 
 function App() {
   return (
@@ -15,13 +16,16 @@ function App() {
       <Banner>
         <Nav />
       </Banner>
-      {/* <Map />
-      <Countries />
-      <Section text="Creative writing" img="images/writing.svg" />
-      <Quote />
-      <Section text="Graphic Design" img="images/trees.svg" />
-      <ContactForm/>
-      <Footer/> */}
+      <main>
+        
+        <Map />
+
+        <Section text="Creative writing" img={writingImg} />
+        <Quote />
+        <Section text="Graphic Design" img={graphicImg} />
+        <ContactForm />
+      </main>
+      <Footer/>
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,37 +1,36 @@
 import React, {useState, useEffect} from "react"
 import logo from "./images/personal-logo.svg"
 import profile from "./images/profile.svg"
+import Parallax from "./Parallax"
 
 function Banner(props) {
-    const [offset, setOffset] = useState({transform: "translateY(0px)"});
-
-    useEffect(()=> {
-        window.addEventListener("scroll", function(e){
-        let rate = -window.pageYOffset *2
-
-        setOffset({transform: `translateY(${rate}px)`})
-        console.log(offset)
-        })
-    }, [])
+    
 
     return (
-        <header className="banner">
-            {props.children}
-            <div class="left-side">
-                <img class="logo" src={logo} alt=""/>
-                <hgroup 
-                style={offset}
-                id="banner-text">
-                    <h2>Hi!</h2>
-                    <h3>>Graphic Designer</h3>
-                    <h3>>Creative Writer</h3>
-                    <h3>>Aspiring Web Dev</h3>
-                </hgroup>
-            </div>
-            <div class="right-side">
-                <img src={profile} alt=""/>
-            </div>
-        </header>
+        <Parallax 
+            rate="1"
+            render={function(para){
+                return (
+                    <header className="banner">
+                        {props.children}
+                        <div className="left-side">
+                            <img className="logo" src={logo} alt=""/>
+                            <hgroup 
+                            style={para}
+                            id="banner-text">
+                                <h2>Hi!</h2>
+                                <h3>-Graphic Designer</h3>
+                                <h3>-Creative Writer</h3>
+                                <h3>-Aspiring Web Dev</h3>
+                            </hgroup>
+                        </div>
+                        <div className="right-side">
+                            <img src={profile} alt=""/>
+                        </div>
+                    </header>
+                )
+        }} />
+        
     )
 }
 
