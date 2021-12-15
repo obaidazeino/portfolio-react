e18c9e7cf5cd8169c8f1598b576140addec01d30
Obaida Zeino <84151630+obaidazeino@users.noreply.github.com>
2021-12-15 09:10:00 -0500

    Make changes

---
+++ src/components/Carousels.js
@@ -0,0 +1,18 @@
+// import React from "react"
+
+// import Carousel from "../components/Parallax"
+// import data from "../data"
+
+// function Carousels() {
+//     console.log(data)
+//     return(
+//         <div>
+//             {data.map(item => {
+//                 return (<Carousel key={item.id} object={item}/>)
+//             })}
+//             {/* <Carousel object={data[2]}/> */}
+//         </div>
+//     )
+// }
+
+// export default Carousels
